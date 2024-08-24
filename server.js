096014f62e290bc4f1ec493a0f97e1c557f9ab68
kiinshuk <red2k2green@gmail.com>
2024-08-24 13:00:18 +0530

    added backend

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
-require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 8000;
-
-// Use CORS to allow requests from all origins
-app.use(cors());
-
-// Define the root route to fetch news based on category
-app.get('/', async (req, res) => {
+module.exports = async (req, res) => {
   const { category } = req.query;
 
   try {
@@ -27,9 +17,4 @@ app.get('/', async (req, res) => {
     console.error('Error fetching news:', error.message);
     res.status(500).json({ message: 'Error fetching news', error: error.message });
   }
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+};
